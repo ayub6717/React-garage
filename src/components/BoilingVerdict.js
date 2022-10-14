7d82610ccd23ad5776da38ddd8dfc98201fd8ca7
Md. Ayub Shamim <ayub15-6717@diu.edu.bd>
2022-10-15 00:22:47 +0600

    Formating

--- src/components/BoilingVerdict.js
+++ src/components/BoilingVerdict.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
 export default function BoilingVerdict({ celsius = 0 }) {
     if (celsius >= 100) {
         return <p>The water would boil.</p>;
