56793df8c87e4b1482406f557aab24ef7bf889de
Md. Ayub Shamim <ayub15-6717@diu.edu.bd>
2022-10-15 00:03:53 +0600

    Lifting-state-up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import React from 'react';
-// import Calculator from './components/Calculator';
-import Todo from './components/Todo';
-import './App.css';
-// import Input from './components/Input';
-// import Clock from './components/Clock';
-// import ClockList from './components/ClockList';
-// import Props from './components/Props';
+import Calculator from './components/Calculator';
 
 function App() {
-    // const quantities = [1, 2, 3];
-    return (
-        <div>
-            {/* <Clock locale="bn-BD" /> */}
-            {/* <Props name="Rohan" />
-            <Props name="Afnan" />
-            <Props name="Ayub" />
-            <Props name="Afnan" />
-            <ClockList quantities={quantities} /> */}
-            {/* <Form /> */}
-            {/* <Calculator /> */}
-            {/* <Input /> */}
-            <Todo />
-        </div>
-    );
+    return <Calculator />;
 }
 
 export default App;
--- src/components/BoilingVerdict.js
+++ src/components/BoilingVerdict.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/prop-types */
 export default function BoilingVerdict({ celsius = 0 }) {
     if (celsius >= 100) {
         return <p>The water would boil.</p>;
