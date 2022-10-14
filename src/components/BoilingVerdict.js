08496583c4274b1746d863e0e72ae1e9eeaddd85
Md. Ayub Shamim <ayub15-6717@diu.edu.bd>
2022-10-15 00:01:54 +0600

    Forms-handling

--- src/components/BoilingVerdict.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/prop-types */
-export default function BoilingVerdict({ celsius = 0 }) {
-    if (celsius >= 100) {
-        return <p>The water would boil.</p>;
-    }
-    return <p>Water would not boil.</p>;
-}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Form extends Component {
+export default class Form extends React.Component {
     state = {
-        title : ''
+        title: 'JavaScript',
+        text: 'JavaScript is awesome',
+        library: 'React',
+        isAwesome: true,
     };
 
-    handleChange = (e) =>{
-        console.log(e.target.value);
-        this.setState({
-            title : e.target.value,
-        })
-    }
+    handleChange = (e) => {
+        if (e.target.type === 'text') {
+            this.setState({
+                title: e.target.value,
+            });
+        } else if (e.target.type === 'textarea') {
+            this.setState({
+                text: e.target.value,
+            });
+        } else if (e.target.type === 'select-one') {
+            this.setState({
+                library: e.target.value,
+            });
+        } else if (e.target.type === 'checkbox') {
+            this.setState({
+                isAwesome: e.target.checked,
+            });
+        } else {
+            console.log('nothing here');
+        }
+    };
 
-  render() {
-    const {title} = this.state
-    return (
-      <div>
-        <form>
-            <input value={title} onChange={this.handleChange} />
-            <p>{title}</p>
-        </form>
-      </div>
-    )
-  }
+    submitHandler = (e) => {
+        const { title, text, library, isAwesome } = this.state;
+        e.preventDefault();
+        console.log(title, text, library, isAwesome);
+    };
+
+    render() {
+        const { title, text, library, isAwesome } = this.state;
+        return (
+            <div>
+                <form onSubmit={this.submitHandler}>
+                    <input type="text" value={null} />
+                    <input
+                        type="text"
+                        placeholder="Enter title"
+                        value={title}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <br />
+                    <textarea name="text" value={text} onChange={this.handleChange} />
+                    <br />
+                    <br />
+                    <select value={library} onChange={this.handleChange}>
+                        <option value="React">React</option>
+                        <option value="Angular">Angular</option>
+                    </select>
+                    <br />
+                    <br />
+                    <input type="checkbox" checked={isAwesome} onChange={this.handleChange} />
+                    <br />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        );
+    }
 }
