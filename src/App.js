cabc7ce8487c435669d0e8f032df8d6339a2db5b
Md. Ayub Shamim <ayub15-6717@diu.edu.bd>
2022-10-15 00:21:12 +0600

    Formating

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import Form from './components/Form';
 
 function App() {
--- src/components/ClockList.js
+++ src/components/ClockList.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
 import Clock from './Clock';
 
 export default function ClockList({ quantities = [] }) {
